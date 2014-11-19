c6b25f1389fa56393d045b3bcdeec02dcf5eba86
Marosi Péter <marosi.peter.k@gmail.com>
2014-11-19 07:52:54 +0100

    Alap refactor

--- config.rb
+++ config.rb
@@ -1,59 +1,35 @@
-# Require any additional compass plugins here.
-
-# Zen Grids.
-
-# If you have installed Zen Grids as a gem, you can require it here.
-# require 'zen-grids'
-
-# Change this to :production when ready to deploy the CSS to the live server.
-#environment = :development
-environment = :production
-
-# In development, we can turn on the FireSass-compatible debug_info.
-firesass = false
-#firesass = true
+# Requre a specific version in this file:
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "img"
-javascripts_dir = "javascripts"
-extensions_dir  = "sass-extensions"
+http_path =         "../"
+css_dir =           "css"
+sass_dir =          "sass"
+images_dir =        "images"
+javascripts_dir =   "js"
+fonts_dir =         "fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
-
+line_comments = true
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+# sass-convert -r grid/ --from scss --to sass
 
+sourcemap = true
 
-##
-## You probably don't need to edit anything below this.
-##
-
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-output_style = (environment == :development) ? :expanded : :compressed
-
-# To enable relative paths to assets via compass helper functions. Since Drupal
-# themes can be installed in multiple locations, we don't need to worry about
-# the absolute path to the theme from the server root.
-relative_assets = false
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-
-# Pass options to sass. For development, we turn on the FireSass-compatible
-# debug_info if the firesass config variable above is true.
-sass_options = (environment == :development && firesass == true) ? {:debug_info => true} : {}
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
--- dhu_html.make
+++
@@ -1,6 +0,0 @@
-api = 2
-core = 7.x
-
-projects[testing][type] = core
-projects[testing][download][type] = git
-projects[testing][download][url] = git@gitlab.i0.hu:dhu_html.git
